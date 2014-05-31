774f11b42e638ca7c605792110bf3765d0f37058
James Davenport <jradavenport@gmail.com>
2014-05-31 00:11:03 -0700

    init

---
+++ rr_charts.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 30 23:06:24 2014
+
+@author: james
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib import cm
+import cubehelix
+
+
+
+
+amt,num,tot = np.loadtxt('rr.txt',unpack=True,skiprows=1)
+
+plt.figure()
+plt.plot(amt,num, 'bo-')
+plt.xscale('log')
+plt.xlabel('Amount per Donation ($)')
+plt.ylabel('# of Backers')
+#plt.yscale('log')
+plt.show()
+
+
+plt.figure()
+plt.plot(amt,tot,'go-')
+plt.xscale('log')
+plt.xlabel('Amount per Donation ($)')
+plt.ylabel('Total Donated per Level ($)')
+plt.show()
+
+
+
+plt.figure()
+plt.plot(num,tot,'ro')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('# of Backers')
+plt.ylabel('Total Donated per Level ($)')
+plt.show()
+
+
+
